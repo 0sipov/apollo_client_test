3aa0421da83e5be23e358ec1daca16fe52518de4
SerhiiOsipov <serhii.osipov@headworks.com.ua>
2021-09-27 17:28:29 +0300

    connect apollo client

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p> loading... </p>;
+
+  if (error) return <p> Error =( </p>;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>This from request</p>
+        <div>
+          {data.rates.map(({ currency, rate }) => (
+            <div key={currency}>
+              <p>
+                {currency}: {rate}
+              </p>
+            </div>
+          ))}
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Apollo client
         </a>
       </header>
     </div>
